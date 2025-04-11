af421dc9df05b083e5ad2701f245f631eb87568b
shmykov.alexander <shmykov.alexander@gmail.com>
2025-04-11 17:05:22 +0300

    add Call for cpp code

--- CallCppFromNet/Program.cs
+++ CallCppFromNet/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,12 @@ namespace CallCppFromNet
 {
     internal class Program
     {
+        [DllImport("CppSourceCodeProject.dll", CallingConvention = CallingConvention.Cdecl/**/)]
+        public static extern int SumInts(int[] a);
         static void Main(string[] args)
         {
+            int[] a = { 1, 2, 3, 4,5,6,7, 0 };
+            Console.WriteLine(SumInts(a));
         }
     }
 }
