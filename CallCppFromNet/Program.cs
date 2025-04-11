92bdfbd7968447586ec8200c5f6dc41a9b4b7fc1
shmykov.alexander <shmykov.alexander@gmail.com>
2025-04-11 18:43:15 +0300

    Added code to load LoadLibrary using kernel32.dll and call it third party code via native  Library

--- CallCppFromNet/Program.cs
+++ CallCppFromNet/Program.cs
@@ -7,18 +7,38 @@ using System.Threading.Tasks;
 
 namespace CallCppFromNet
 {
+    static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procedureName);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
+    }
     internal class Program
     {
         private const string CppLib = "CppSourceCodeProject.dll";
 
         [DllImport(CppLib, CallingConvention = CallingConvention.Cdecl/**/)]
         public static extern int SumInts(int[] a);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate int SumIntsDelegate(int[] numberToSum);
         static void Main(string[] args)
         {
             int[] a = { 1, 2, 3, 4,5,6,7,8, 0 };
 
             string separator = " + ";
             Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator,a)}) = {SumInts(a)}");
+            IntPtr pDll = NativeMethods.LoadLibrary(CppLib);
+            IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, $"{nameof(SumInts)}");
+            var SumIntsDelegateObj = (SumIntsDelegate)Marshal.GetDelegateForFunctionPointer(
+                                                                                    pAddressOfFunctionToCall,
+                                                                                    typeof(SumIntsDelegate));
+            Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");         
         }
     }
 }
