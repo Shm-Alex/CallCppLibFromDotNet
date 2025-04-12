395231d3cb5b4b8524ee05ef0c2f0ba010a6a5c9
shmykov.alexander <shmykov.alexander@gmail.com>
2025-04-12 16:39:44 +0300

    Добавлен кросплатформенный  проект который  не собирает в результате  CppSourceCodeProject.dll В нужном виде , при загрузке библиотеки происходит исключение  должен зафиксирповать версию  для  последующих экспирементов

---
+++ CrossPlatformConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+using DotNetStandardCppWrapper;
+namespace CrossPlatformConsole
+{
+    internal class Program
+    {
+
+            private const string CppLib = "CppSourceCodeProject.dll";
+
+            [DllImport(CppLib, CallingConvention = CallingConvention.Cdecl/**/)]
+            public static extern int SumInts(int[] a);
+
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            private delegate int SumIntsDelegate(int[] numberToSum);
+            static void Main(string[] args)
+            {
+                int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
+
+                string separator = " + ";
+                Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");
+                IntPtr pDll = NativeMethods.LoadLibrary(CppLib);
+                IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, $"{nameof(SumInts)}");
+                var SumIntsDelegateObj = (SumIntsDelegate)Marshal.GetDelegateForFunctionPointer(
+                                                                                        pAddressOfFunctionToCall,
+                                                                                        typeof(SumIntsDelegate));
+                using (SumIntsCallWrapper cppDll = new SumIntsCallWrapper(CppLib))
+                {
+                    Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {cppDll.SumInts(a)}");
+                }
+
+                Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");
+            }
+    }
+    
+}
