1305e2c0638eb1c5a447b7ff05d11d86f09f6651
shmykov.alexander <shmykov.alexander@gmail.com>
2025-04-13 18:15:44 +0300

    Doker Broken Some fixes

--- CrossPlatformConsole/Program.cs
+++ CrossPlatformConsole/Program.cs
@@ -19,7 +19,7 @@ namespace CrossPlatformConsole
             int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
 
             string separator = " + ";
-            Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");
+            Console.WriteLine($@"Line 22 Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");
             #endregion
 
             #region  manual loading dll to memory using system32 LoadLibrary and   Call SumInts from CppLib and call SumInts  binding delegete SumIntsDelegate  with  cpp code via   GetDelegateForFunctionPointer  
@@ -28,14 +28,14 @@ namespace CrossPlatformConsole
             var SumIntsDelegateObj = (SumIntsDelegate)Marshal.GetDelegateForFunctionPointer(
                                                                                     pAddressOfFunctionToCall,
                                                                                     typeof(SumIntsDelegate));
-            Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumInts(a)}");
+            Console.WriteLine($@"Line 31 Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {SumIntsDelegateObj(a)}");
 
             #endregion
             #region same as previous method, but using  IDisposable wrapper class
 
             using (IMathLibService cppDll = new SumIntsCallWrapper(CppLib))
             {
-                Console.WriteLine($@"Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {cppDll.SumInts(a)}");
+                Console.WriteLine($@"line 38  Call cpp code from {CppLib} {nameof(SumInts)} ({String.Join(separator, a)}) = {cppDll.SumInts(a)}");
             }
             #endregion
         }
